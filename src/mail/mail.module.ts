b5fd0d7f0229f343eedcfad226991cb3ccafb240
Joel Dominguez <joezlcell@gmail.com>
2026-05-14 21:39:39 -0300

    try to resend SMTP

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -5,32 +5,33 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { PugAdapter } from '@nestjs-modules/mailer/adapters/pug.adapter'
 import { join } from 'path';
 @Module({
-  imports: [MailerModule.forRootAsync({
-    useFactory: (): any => ({
-      transport: {
-          host: process.env.SMTP_HOST,
-          port: process.env.SMTP_PORT,
-          secure: true,
-          auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
-          },
-          tls: {
-            rejectUnauthorized: false,
-          }
-      },
-      defaults: {
-        from: process.env.SMTP_FROM,
-      },
-      template: {
-        dir: join(__dirname,'..', '..', 'templates'),
-        adapter: new PugAdapter(),
-        options: {
-          strict: true,
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: (): any => ({
+        transport: {
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: true,
+            auth: {
+              user: process.env.EMAIL_USER,
+              pass: process.env.EMAIL_PASSWORD
+            },
+            tls: {
+              rejectUnauthorized: false,
+            }
         },
-      }
+        defaults: {
+          from: process.env.SMTP_FROM,
+        },
+        template: {
+          dir: join(__dirname,'..', '..', 'templates'),
+          adapter: new PugAdapter(),
+          options: {
+            strict: true,
+          },
+        }
+      })
     })
-  })
   ],
   controllers: [MailController],
   providers: [MailService],
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,25 +1,38 @@
 import { ISendMailOptions, MailerService } from '@nestjs-modules/mailer';
 import { Injectable, Logger } from '@nestjs/common';
-
+import * as path from 'path';
+import * as pug from 'pug';
+import { Resend } from 'resend';
 @Injectable()
 export class MailService {
     constructor(private readonly mailerService: MailerService) {}
     private logger = new Logger('User service');
+    private resend = new Resend(process.env.RESEND_API_KEY);
 
     async sendEmail(params: {to: string, subject: string, template: string, context: ISendMailOptions['context']}) {
         this.logger.log(`Sending email...`);
         try {
-            const mailParams = {
+            /* const mailParams = {
                 from: process.env.SMTP_FROM,
                 to: params.to,
                 subject: params.subject,
                 template: params.template,
                 context: params.context
+            }; 
+
+            const response = await this.mailerService.sendMail(mailParams); */
+            
+            const templatePath = path.join(__dirname,'..', '..', `templates/${params.template}.pug`);
+            const html = pug.renderFile(templatePath, { ...params.context });
+
+            const mailParams = {
+                from: process.env.RESEND_DOMAIN ?? '',
+                to: params.to,
+                subject: params.subject,
+                html: html
             };
 
-            console.log('----------------->',this.mailerService.getTransporter().options);
-
-            const response = await this.mailerService.sendMail(mailParams);
+            const response = await this.resend.emails.send(mailParams);
 
             this.logger.log(
                         `Email sent successfully to recipients with the following parameters : ${JSON.stringify(
